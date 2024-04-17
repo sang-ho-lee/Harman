f0e9520a3e94d7e73df0e8269e4553f9ce358853
Sangho Lee <leesh2540@gmail.com>
2024-04-17 09:54:40 +0900

    4/17

---
+++ ultrasonic/build/CMakeFiles/ultrasonic.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ultrasonic.dir/main.c.obj"
+  "CMakeFiles/ultrasonic.dir/main.c.obj.d"
+  "ultrasonic"
+  "ultrasonic.manifest"
+  "ultrasonic.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ultrasonic.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ultrasonic/build/CMakeFiles/ultrasonic.elf.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ultrasonic.elf.dir/main.c.obj"
+  "CMakeFiles/ultrasonic.elf.dir/main.c.obj.d"
+  "ultrasonic.elf"
+  "ultrasonic.elf.manifest"
+  "ultrasonic.elf.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ultrasonic.elf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
